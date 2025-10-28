e8a7ff40a01093766798fd4b8d6f14c9fcf2cb2e
Killian Morere <killian.morere@fms-ea.com>
2025-10-28 16:01:01 +0100

    Added main functions and program.

--- src/tp2/Restaurant.java
+++ src/tp2/Restaurant.java
@@ -1,8 +1,49 @@
 package tp2;
 
+import java.util.Scanner;
+
 public class Restaurant {
 
+	static Scanner scan = new Scanner(System.in);
+	static String[][] menu = {
+			{"None", "Salad", "Soup", "Quiche"},
+			{"None", "Chicken", "Beef", "Fish", "Vegan"},
+			{"None", "Rice", "Pasta", "Fries", "Vegetables"},
+			{"None", "Water", "Soda", "Wine"}
+	};
+	
 	public static void main(String[] args) {
+		startOrder();
 		
+		scan.close();
+	}
+	
+	private static void startOrder() {
+		System.out.println("Hello, how many orders do you want ?");
+		int amount = scan.nextInt();
+		
+		String[] order = new String[menu.length];
+		for(int i = 0; i < menu.length; i++)
+			order[i] = selectOrder(i);
+
+		System.out.print("[");
+		for(String ord : order) {
+			System.out.print(ord);
+			System.out.print(" ");
+		}
+		System.out.print("]");
+		System.out.println();
+	}
+	
+	private static String selectOrder(int index) {
+		for(int i = 0; i < menu[index].length; i++) {
+			System.out.print("[" + i + " - " + menu[index][i] + "]");
+		}
+		System.out.println();
+		
+		System.out.println("Select a number :");
+		int input = scan.nextInt();
+		
+		return menu[index][input];
 	}
 }
