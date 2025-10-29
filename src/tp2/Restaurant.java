02d88cb6f94c43986ee8fcfefa0814c394c9f308
Killian Morere <killian.morere@fms-ea.com>
2025-10-29 08:47:24 +0100

    Added comments, input verification for a valid order and fixed the program only ordering once.

--- src/tp2/Restaurant.java
+++ src/tp2/Restaurant.java
@@ -5,23 +5,26 @@ import java.util.Scanner;
 public class Restaurant {
 
 	static Scanner scan = new Scanner(System.in);
-	static String[][] menu = {
+	static String[][] menu = { // 2 Dimensionnal array of the menu.
 			{"None", "Salad", "Soup", "Quiche"},
 			{"None", "Chicken", "Beef", "Fish", "Vegan"},
 			{"None", "Rice", "Pasta", "Fries", "Vegetables"},
-			{"None", "Water", "Soda", "Wine"}
+			{"None", "Water", "Soda", "Wine"},
+			{"None", "Pie", "Chocolate Mousse", "Tiramisu"},
 	};
 	
 	public static void main(String[] args) {
-		startOrder();
+		System.out.println("Hello, how many orders do you want ?");
+		int amount = scan.nextInt();
+		
+		for (int i = 0; i < amount; i++) // Do as many orders as requested beforehand.
+			startOrder();
 		
 		scan.close();
 	}
 	
+	// Initialize an order.
 	private static void startOrder() {
-		System.out.println("Hello, how many orders do you want ?");
-		int amount = scan.nextInt();
-		
 		String[] order = new String[menu.length];
 		for(int i = 0; i < menu.length; i++)
 			order[i] = selectOrder(i);
@@ -35,6 +38,11 @@ public class Restaurant {
 		System.out.println();
 	}
 	
+	/**
+	 * Selection of an element from the menu array.
+	 * @param index Index parameter given from startOrder() for the selected order.
+	 * @return Returns the element from the menu array.
+	 */
 	private static String selectOrder(int index) {
 		for(int i = 0; i < menu[index].length; i++) {
 			System.out.print("[" + i + " - " + menu[index][i] + "]");
@@ -43,6 +51,9 @@ public class Restaurant {
 		
 		System.out.println("Select a number :");
 		int input = scan.nextInt();
+		
+		if (input < 0 && input >= menu[index].length)
+			return menu[0][0]; // Return None in the menu if the input is not valid.
 		
 		return menu[index][input];
 	}
